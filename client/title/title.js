35833550b8881300c234de3737e75dbcf9fdf814
juh1128 <juh17@naver.com>
2015-05-17 17:37:22 +0900

    add 150517

--- client/title/title.js
+++ client/title/title.js
@@ -33,6 +33,14 @@ function noticeInit(){
 
     slideData = noticeSlideImages.find( {} );
 
+    //SlideImage의 자식 요소로 thumbnail 나열
+    slideData.forEach( function(doc){
+        if(doc){
+            $('.slideImage').
+        }
+    });
+
+
     //스플래시 이미지 초기화
     nowSelected = -1;
     updateSplash();
